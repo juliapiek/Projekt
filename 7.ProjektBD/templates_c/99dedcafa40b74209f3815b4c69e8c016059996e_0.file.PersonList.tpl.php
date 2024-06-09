92a8c087f1c6db5866b48adea604b7c7dbc0121a
Juliapiek <piekarskajulia337@interia.pl>
2024-06-09 13:26:57 +0200

    .

---
+++ 7.ProjektBD/templates_c/99dedcafa40b74209f3815b4c69e8c016059996e_0.file.PersonList.tpl.php
@@ -0,0 +1,113 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-05-07 09:35:57
+  from "C:\xampp\htdocs\7.ProjektBD\app\views\PersonList.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6639d9dd3c96e0_83863653',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '99dedcafa40b74209f3815b4c69e8c016059996e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\7.ProjektBD\\app\\views\\PersonList.tpl',
+      1 => 1715062976,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:main.tpl' => 1,
+  ),
+),false)) {
+function content_6639d9dd3c96e0_83863653 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_6702826226639d9dd3be859_23351434', 'top');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_18468610416639d9dd3c8ee0_62107720', 'bottom');
+?>
+
+<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'top'} */
+class Block_6702826226639d9dd3be859_23351434 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div class="bottom-margin">
+<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personList">
+	<legend>Opcje wyszukiwania</legend>
+	<fieldset>
+		<input type="text" placeholder="nazwisko" name="sf_surname" value="<?php echo $_smarty_tpl->tpl_vars['searchForm']->value->surname;?>
+" /><br />
+		<button type="submit" class="pure-button pure-button-primary">Filtruj</button>
+	</fieldset>
+</form>
+</div>	
+
+<?php
+}
+}
+/* {/block 'top'} */
+/* {block 'bottom'} */
+class Block_18468610416639d9dd3c8ee0_62107720 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<div class="bottom-margin">
+<a class="pure-button button-success" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+personNew">+ Nowa osoba</a>
+</div>	
+
+<table id="tab_people" class="pure-table pure-table-bordered">
+<thead>
+	<tr>
+		<th>imię</th>
+		<th>nazwisko</th>
+		<th>data ur.</th>
+		<th>opcje</th>
+	</tr>
+</thead>
+<tbody>
+<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['people']->value, 'p');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['p']->value) {
+?>
+<tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
+</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
+</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["birthdate"];?>
+</td><td><a class="button-small pure-button button-secondary" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personEdit&id=<?php echo $_smarty_tpl->tpl_vars['p']->value['idperson'];?>
+">Edytuj</a>&nbsp;<a class="button-small pure-button button-warning" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+personDelete&id=<?php echo $_smarty_tpl->tpl_vars['p']->value['idperson'];?>
+">Usuń</a></td></tr>
+<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+</tbody>
+</table>
+
+<?php
+}
+}
+/* {/block 'bottom'} */
+}
